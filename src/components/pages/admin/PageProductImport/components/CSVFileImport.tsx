a38d4fa33ec5fc3fbd5866be4939fcfc7a0a7e2d
s.fomin <s.fomin@softteco.com>
2025-03-24 18:19:20 +0300

    task-7 fix auth

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -31,6 +31,9 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
       if (!file) {
         throw new Error("No file selected");
       }
+
+      const authToken = localStorage.getItem("authorization_token");
+
       // Get the presigned URL
       const response = await axios({
         method: "GET",
@@ -38,9 +41,11 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
         params: {
           name: encodeURIComponent(file.name),
         },
-        headers: {
-          Authorization: `Basic ${localStorage.getItem("authorization_token")}`,
-        },
+        headers: authToken
+          ? {
+              Authorization: `Basic ${authToken}`,
+            }
+          : {},
       });
       console.log("File to upload: ", file.name);
       console.log("Uploading to: ", response.data);
